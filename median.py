ef447aace82b891f13f30cb7a1ca76ee827dc7ef
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-20 10:33:07 +0200

    adding median calculator.

---
+++ median.py
@@ -0,0 +1,7 @@
+def get_median():
+    number_array = list()
+    number_index = input("Inserisci il numero dei numeri: ") 
+    if number_index % 2 == 1:
+        return number_array[numeber_index/2]
+    elif number_index % 2 == 0:
+        

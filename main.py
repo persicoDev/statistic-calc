385e9f91eea8c7ce8f6bc96ad2b4a7154dc05a71
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-21 10:50:29 +0200

    code refactoring.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-import average
-import median
-import mode
-import standard_deviation
+from type_calc.average  import get_average
+from type_calc.median import get_median
+from type_calc.mode import get_mode
+from type_calc.standard_deviation import get_standard_deviation
 
 def menu():
-    choice = int(input('1- esegui la media\n2- esegui la mediana\n3- esegui la moda\n4- esegui lo scarto quadratico medio: '))
-    if choice == 1:
-        print(average.get_average())
-    if choice == 2:
-        print(median.get_median())
-    if choice == 3:
-        print(mode.get_mode())
-    if choice == 4: 
-        print(standard_deviation.get_standard_deviation())
+    while True:
+        print('-------------------------------------------------')
+        choice = int(input('\t1- esegui la media\n\t2- esegui la mediana\n\t3- esegui la moda\n\t4- esegui lo scarto quadratico medio\n\t0- uscita dal programma.\n\tinserimento:  '))
+        if choice == 1:
+            print(get_average())
+        if choice == 2:
+            print(get_median())
+        if choice == 3:
+            print(get_mode())
+        if choice == 4: 
+            print(get_standard_deviation())
+        if choice == 0:
+            break
 
 if __name__ == '__main__':
     menu()

e209c327c2d824140f0661882c08a56e5a7a465e
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-05-21 09:49:16 +0200

    fixed bug.

--- mode.py
+++ mode.py
@@ -2,7 +2,7 @@ from statistics import mode
 
 def get_mode():
     number_array = list()
-    number_index = input("Inserisci il numero dei numeri: ") 
+    number_index = int(input("Inserisci il numero dei numeri: "))
     for i in range(number_index):
         number_array.append(float(input("Inserisci il numero : ")))
     return (mode(number_array))
